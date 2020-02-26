dee3893994267913563a8a5c0435a71f0db2c045
Pasha <88pasha88@gmail.com>
2020-02-27 00:07:10 +0200

    base routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, NavLink, Route, Switch} from "react-router-dom";
+import {CreateNewTask} from "./pages/CreateNewTask";
+import {TodoList} from "./pages/TodoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <BrowserRouter>
+            <NavLink to={"/"}>ToDo list.</NavLink>
+            <NavLink to={"/newTask"}>Create new task.</NavLink>
+            <Switch>
+                <Route exact path={"/"} render={(props) => (<TodoList props={props}/>)}/>
+                <Route exact path={"/newTask"} render={(props) => (<CreateNewTask props={props}/>)}/>
+            </Switch>
+        </BrowserRouter>
+    );
+};
 
 export default App;
---
+++ src/pages/CreateNewTask/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const CreateNewTask = () => {
+    return (
+        <div>CreateNewTask page</div>
+    )
+};
