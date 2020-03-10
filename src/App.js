c656243edf1a7bc69a0243159d02f86834c89c65
k0xxx <aleksandr.k@powercode.us>
2020-03-10 15:37:17 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
-import React from 'react';
-import {BrowserRouter, NavLink, Route, Switch} from "react-router-dom";
-import {createStore} from "redux";
+import React from "react";
+import { BrowserRouter, NavLink, Route, Switch } from "react-router-dom";
+import { createStore } from "redux";
 import Provider from "react-redux/lib/components/Provider";
 
-
 //reducers
-import {todo} from "./redux/Todos/reducer";
+import { todo } from "./redux/Todos/reducer";
 
 //components
-import {NewToDo} from "./pages/NewToDo/NewToDo";
-import {TodoList} from "./pages/TodoList";
+import { NewToDo } from "./pages/NewToDo/NewToDo";
+import { TodoList } from "./pages/TodoList";
 
-
-export const store = createStore(
-    todo,
-);
+export const store = createStore(todo);
 
 export const App = () => {
-    return (
-        <Provider store={store}>
-            <BrowserRouter>
-                <NavLink to={"/"}>ToDo list.</NavLink>
-                <NavLink to={"/createTodo"}>Create new task.</NavLink>
-                <Switch>
-                    <Route exact path={"/"} component={TodoList}/>)}/>
-                    <Route exact path={"/createTodo"} render={() => (<NewToDo/>)}/>
-                </Switch>
-            </BrowserRouter>
-        </Provider>
-    );
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <NavLink to={"/"}>ToDo list.</NavLink>
+        <NavLink to={"/createTodo"}>Create new task.</NavLink>
+        <Switch>
+          <Route exact path={"/"} component={TodoList} />
+          <Route exact path={"/createTodo"} render={() => <NewToDo />} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+  );
 };
 
 export default App;
--- src/components/CreateNewToDo/CreateNewTodo.js
+++ src/components/CreateNewToDo/CreateNewTodo.js
@@ -1,51 +1,54 @@
-import React, {useState} from "react";
-import {CreateNewTask} from "../../redux/Todos/action";
+import React, { useState } from "react";
+import { CreateNewTask } from "../../redux/Todos/action";
 import connect from "react-redux/lib/connect/connect";
 
+const CreateNewTodo = ({ dispatch }) => {
+  const [title, setTitle] = useState("");
+  const [fullText, setFullText] = useState("");
+  const [description, setDescription] = useState("");
 
-const CreateNewTodo = ({dispatch}) => {
+  const SubmitToDo = event => {
+    event.preventDefault();
+    if (title.length > 3 || fullText.length > 3) {
+      const post = {
+        title: title,
+        fullText: fullText,
+        description: description,
+      };
 
-    const [title, setTitle] = useState('');
-    const [fullText, setFullText] = useState('');
-    const [description, setDescription] = useState('');
+      dispatch(CreateNewTask(post));
 
-    const SubmitToDo = (event) => {
-        event.preventDefault();
-        if (title.length > 3 || fullText.length > 3) {
+      setTitle("");
+      setFullText("");
+      setDescription("");
+      console.log(post);
+    }
+  };
 
-            const post = {
-                title: title,
-                fullText: fullText,
-                description: description,
-            };
-
-            dispatch(CreateNewTask(post));
-
-            setTitle('');
-            setFullText('');
-            setDescription('');
-            console.log(post)
-        }
-    };
-
-    return (
-        <form onSubmit={SubmitToDo}>
-            <div>
-                Title:
-                <input placeholder={"Title"} value={title} type="text" onChange={e => setTitle(e.target.value)}/>
-            </div>
-            <div>
-                Full text:
-                <input placeholder={"Text"} value={fullText} type="text" onChange={e => setFullText(e.target.value)}/>
-            </div>
-            <div>
-                Description:
-                <input placeholder={"Description"} value={description} type="text"
-                       onChange={e => setDescription(e.target.value)}/>
-            </div>
-            <button disabled={(title.length < 3 || fullText.length < 3)} onClick={SubmitToDo}>Create todos</button>
-        </form>
-    )
+  return (
+    <form onSubmit={SubmitToDo}>
+      <div>
+        Title:
+        <input placeholder={"Title"} value={title} type="text" onChange={e => setTitle(e.target.value)} />
+      </div>
+      <div>
+        Full text:
+        <input placeholder={"Text"} value={fullText} type="text" onChange={e => setFullText(e.target.value)} />
+      </div>
+      <div>
+        Description:
+        <input
+          placeholder={"Description"}
+          value={description}
+          type="text"
+          onChange={e => setDescription(e.target.value)}
+        />
+      </div>
+      <button disabled={title.length < 3 || fullText.length < 3} onClick={SubmitToDo}>
+        Create todos
+      </button>
+    </form>
+  );
 };
 
-export default connect(() => ({}))(CreateNewTodo)
+export default connect(() => ({}))(CreateNewTodo);
