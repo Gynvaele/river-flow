2c2c8c42238e9478afe3d44f3d5e98b12a3ba4a8
Pasha <88pasha88@gmail.com>
2020-03-12 02:47:13 +0200

    material navs

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,19 +1,29 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./style.scss";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 
 export const Nav = () => {
   return (
     <div id={"navigation"}>
-      <NavLink exact to={"/"} className={"navButton"}>
-        SignIn page
-      </NavLink>
-      <NavLink exact to={"/todos"} className={"navButton"}>
-        ToDo list.
-      </NavLink>
-      <NavLink exact to={"/createTodo"} className={"navButton"}>
-        Create new task.
-      </NavLink>
+      <ButtonGroup variant="text" color="primary" aria-label="text primary button group">
+        <Button>
+          <NavLink exact to={"/"} className={"navButton"}>
+            SignIn page
+          </NavLink>
+        </Button>
+        <Button>
+          <NavLink exact to={"/todos"} className={"navButton"}>
+            ToDo list.
+          </NavLink>
+        </Button>
+        <Button>
+          <NavLink exact to={"/createTodo"} className={"navButton"}>
+            Create new task.
+          </NavLink>
+        </Button>
+      </ButtonGroup>
     </div>
   );
 };
