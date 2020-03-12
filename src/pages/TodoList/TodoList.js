a32c6bafcf6978c5dcb17876649fb634ada9c387
Pasha <88pasha88@gmail.com>
2020-03-13 01:16:09 +0200

    modal create task

--- src/pages/TodoList/TodoList.js
+++ src/pages/TodoList/TodoList.js
@@ -1,12 +1,14 @@
 import React from "react";
 import TodoTasks from "../../components/Todos/TodoTasks";
 import StartedTasks from "../../components/StartedTasks/StartedTasks";
+import { NewTaskModal } from "../../components/CreateNewToDo/openModal";
 
 export const TodoList = () => {
   return (
     <div>
       <TodoTasks />
       <StartedTasks />
+      <NewTaskModal />
     </div>
   );
 };
