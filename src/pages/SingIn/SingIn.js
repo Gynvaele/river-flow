2e8574449d9180edbb2343609f94ada8cb6c127b
Pasha <88pasha88@gmail.com>
2020-03-15 00:48:26 +0200

    SingIn page styled

--- src/pages/SingIn/SingIn.js
+++ src/pages/SingIn/SingIn.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import "./style.scss";
+import { TextField } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
 
 export const SingIn = () => {
   const [login, setLogin] = useState("");
@@ -17,19 +19,26 @@ export const SingIn = () => {
   };
   return (
     <div id="SignIn">
-      <div className="container">
-        <form onSubmit={Login}>
-          <div>
-            <span>Login:</span>
-            <input type="text" value={login} onChange={e => setLogin(e.target.value)} />
-          </div>
-          <div>
-            <span>Password:</span>
-            <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-          </div>
-          <button onClick={Login}>Confirm</button>
-        </form>
-      </div>
+      <form onSubmit={Login} className="container">
+        <TextField
+          type="text"
+          label={"Login:"}
+          value={login}
+          onChange={e => setLogin(e.target.value)}
+          className={"singIn-input"}
+        />
+        <TextField
+          type="password"
+          label={"Password:"}
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          className={"singIn-input"}
+        />
+        <Button onClick={Login} variant={"contained"} size={"small"} color={"primary"} className={"sign-button"}>
+          Sign In
+        </Button>
+      </form>
+      {/*container for weather background*/}
       {/*<canvas id="rainfall" width="800" height="600" style={{ border: "1px solid black" }}></canvas>*/}
     </div>
   );
